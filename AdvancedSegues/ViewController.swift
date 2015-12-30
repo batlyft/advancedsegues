c7838c0a4e6bca44c89b94850d02ddb966bc741f
Bat Computer <Batcomputer@Bats-MacBook-Pro.local>
2015-12-30 10:00:05 -0800

    first commit

---
+++ AdvancedSegues/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AdvancedSegues
+//
+//  Created by Bat Computer on 10/16/15.
+//  Copyright © 2015 HardSoft. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(rowCounter)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
